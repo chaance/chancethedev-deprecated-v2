7f60361456fdec40be29c15b38fc61086a9f4660
Chance Strickland <hi@chancedigital.io>
2019-09-27 12:21:51 -0700

    mv helmet + add module resolution fix

--- gatsby-node.js
+++ gatsby-node.js
@@ -5,7 +5,7 @@ exports.onCreateWebpackConfig = ({ config, actions }) => {
     resolve: {
       alias: {
         'react-dom': '@hot-loader/react-dom',
-        'react-helmet': path.join(__dirname, './src/components/helmet'),
+        'react-helmet': path.join(__dirname, './src/components/react-helmet'),
         $src: path.join(__dirname, './src'),
         $components: path.join(__dirname, './src/components'),
         $images: path.join(__dirname, './src/images'),
